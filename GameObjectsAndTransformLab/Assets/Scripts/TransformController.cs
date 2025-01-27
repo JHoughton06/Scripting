01fce4932d75dbc8f7d2313d2761f982f6dd7bcd
JHoughton06 <pandahugger573@gmail.com>
2025-01-26 19:54:54 -0700

    finished Lab 2

---
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TansformController : MonoBehaviour
+{
+    private void Update()
+    {
+        //Moving the game object
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+
+        // rotate the game object
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    }
+
+}
+
